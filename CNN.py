9f0ac03936b44f71562e2ee2f0d7073138cc6a1e
Grimm222 <kostya20040831@gmail.com>
2024-11-25 23:23:03 +0300

    Update CNN.py
    Код нейросети модели CNN

--- CNN.py
+++ CNN.py
@@ -6,12 +6,14 @@ import torch.nn as nn
 import torch.utils.data as data
 import torch.nn.functional as F
 import PIL
-from PIL import Image
+from PIL import Image, ImageFile
+import timm
+ImageFile.LOAD_TRUNCATED_IMAGES = True
 
 
-train_data_path = "Q:\\train"
-val_data_path = "Q:\\val"
-test_data_path = "Q:\\test"
+train_data_path = "D:\\train"
+val_data_path = "D:\\val"
+test_data_path = "D:\\test"
 
 
 class Dataset(object):
@@ -20,13 +22,13 @@ class Dataset(object):
 	def __len__(self):
 		raise NotImplementedError
 
-transforms = transforms.Compose([transforms.Resize(64),	transforms.ToTensor(),transforms.Normalize(mean=[0.485, 0.456, 0.406], std = [0.229, 0.224, 0.225])])
+transforms = transforms.Compose([transforms.Resize([224,224],interpolation=PIL.Image.BICUBIC),transforms.ToTensor(),transforms.Normalize(mean=[0.485, 0.456, 0.406], std = [0.229, 0.224, 0.225])])
 
 train_data = torchvision.datasets.ImageFolder(root=train_data_path, transform=transforms)
 val_data = torchvision.datasets.ImageFolder(root=val_data_path,transform=transforms)
 test_data = torchvision.datasets.ImageFolder(root=test_data_path,transform=transforms)
 
-batch_size=64
+batch_size=128
 
 train_data_loader = data.DataLoader(train_data, batch_size=batch_size)
 val_data_loader = data.DataLoader(val_data, batch_size=batch_size)
@@ -34,9 +36,8 @@ test_data_loader = data.DataLoader(test_data, batch_size=batch_size)
 
 
 
-class CNN(nn.Module):
-	def __init__(self,x, num_classes=2):
-
+class SwTr(nn.Module):
+	def __init__(self,x, num_classes=3):
 		super().__init__()
 		self.features = nn.Sequential(
 			nn.Conv2d(3, 64, kernel_size=11, stride=4, padding=2),
@@ -70,23 +71,40 @@ class CNN(nn.Module):
 		x = self.classifier(x)
 		return x
 
-y=torch.rand(2)
-simpl = CNN(y)
+if (torch.cuda.is_available()):
+	device ='cuda'
+else:
+	device ='cpu'
 
+y=torch.rand(2)
+
+#Initializing a class  simpl
+simpl = SwTr(y)
+print("Do you want to re-train the model (0) or use ready-made weights and parameters(1)? ")
+
+b = input()
+a = int(b)
+
+if a>0:
+	simpl.load_state_dict(torch.load('D:\\swin_transformer_model.pth',weights_only=True))
+	simpl.eval() 
+	simpl.to(device)
 
 optimizer = optim.Adam(simpl.parameters(), lr=0.001)
 
-training_loss =0.0
-valid_loss =0.0
-
-def train(model, optimizer, loss_fn, train_loader, val_loader, device="cpu"):
-	epochs = 600
+def train(model, optimizer, loss_fn, train_loader, val_loader, device):
+	epochs = 10
 	train_iterator =0
 	valid_iterator =0
+	num_correct = 0
+	num_examples = 0
 	for epoch in range(epochs):
 		training_loss = 0.0
 		valid_loss = 0.0
 		model.train()
+		if (torch.cuda.is_available()):
+			model.cuda()
+
 		for batch in train_loader:
 			optimizer.zero_grad()
 			inputs, targets = batch
@@ -101,102 +119,83 @@ def train(model, optimizer, loss_fn, train_loader, val_loader, device="cpu"):
 		training_loss /= train_iterator
 
 		model.eval()
-		num_correct = 0
-		num_examples = 0
-		for batch in val_loader:
+		if (torch.cuda.is_available()):
+			model.cuda()
+		with torch.no_grad():
+			for batch in val_loader:
+				inputs, targets = batch
+				inputs = inputs.to(device)
+				output = model(inputs)
+				targets = targets.to(device)
+				loss = loss_fn(output,targets)
+				valid_loss += loss.data.item()
+				correct = torch.eq(torch.max(F.log_softmax(output,1),1)[1], targets).view(-1)
+				num_correct += torch.sum(correct).item()
+				num_examples += correct.shape[0]
+				valid_iterator+=1
+			valid_loss /= valid_iterator
+		print('E: {:2}, Training Loss: {:.10f}, Validation Loss: {:.10f}, accuracy = {:.2f}, {:}/{:}'.format(epoch+1, training_loss, valid_loss, num_correct / num_examples, num_correct, num_examples))
+
+	torch.save(model.state_dict(), 'D:\\swin_transformer_model.pth')
+	print("Model has been saved!")
+
+
+
+def test(model, test_loader, device):
+	num_correct = 0
+	num_examples = 0
+	if (torch.cuda.is_available()):
+			model.cuda()
+	with torch.no_grad():
+		for batch in test_loader:
 			inputs, targets = batch
 			inputs = inputs.to(device)
 			output = model(inputs)
 			targets = targets.to(device)
-			loss = loss_fn(output,targets)
-			valid_loss += loss.data.item()
-			correct = torch.eq(torch.max(F.log_softmax(output,1),1)[1], targets).view(-1)
+			correct = torch.eq(torch.max(F.log_softmax(output, 1), 1)[1], targets).view(-1)
 			num_correct += torch.sum(correct).item()
 			num_examples += correct.shape[0]
-			valid_iterator+=1
-		valid_loss /= valid_iterator
-		print('Epoch: {}, Training Loss: {:.10f}, Validation Loss: {:.10f}, accuracy = {:.2f}'.format(epoch+1, training_loss, valid_loss, num_correct / num_examples))
+			accuracy = num_correct / num_examples
+		print(f'Accuracy on test set: {accuracy:.2f}, {num_correct}/{num_examples}')
 
 
-device ='cpu'
-train(simpl, optimizer, torch.nn.CrossEntropyLoss(), train_data_loader, test_data_loader, device)
 
 
-labels = ['volk','zaec']
+if a>0:
+	test(simpl, test_data_loader, device)
+else:
+	train(simpl, optimizer, torch.nn.CrossEntropyLoss(), train_data_loader, val_data_loader, device)
 
-img1 = Image.open('D:\\volchara.jpg')			 
+	
+
+labels = ['blue','green','red']
+
+img1 = Image.open('D:\\blue1.png').convert('RGB')			 
 img1 = transforms(img1)
 img1 = img1.unsqueeze(0)
 
-img2 = Image.open('D:\\zaeca.jpg')			 
+img2 = Image.open('D:\\green1.png').convert('RGB')			 
 img2 = transforms(img2)
 img2 = img2.unsqueeze(0)
 
-
-#volk
-prediction = simpl(img1)
-prediction = prediction.argmax()
-print(labels[prediction])
-
-#zaec
-prediction = simpl(img2)
-prediction = prediction.argmax()
-print(labels[prediction])
-
-
-'''
-##########################################
-img1 = Image.open('D:\\Krug.jpg')			 
-img1 = transforms(img1)
-img1 = img1.unsqueeze(0)
-
-img2 = Image.open('D:\\Treugolnik.jpg')			 
-img2 = transforms(img2)
-img2 = img2.unsqueeze(0)
-
-img3 = Image.open('D:\\O.jpg')			 
+img3 = Image.open('D:\\red1.png').convert('RGB')			 
 img3 = transforms(img3)
 img3 = img3.unsqueeze(0)
 
-img4 = Image.open('D:\\TR.jpg')			 
-img4 = transforms(img4)
-img4 = img4.unsqueeze(0)
-
-img6 = Image.open('D:\\triangle.jpg')			 
-img6 = transforms(img6)
-img6 = img6.unsqueeze(0)
-
-img7 = Image.open('D:\\kvadrat.jpg')			 
-img7 = transforms(img7)
-img7 = img7.unsqueeze(0)
-
-#circle
+if (torch.cuda.is_available()):
+	img1 = img1.cuda()
 prediction = simpl(img1)
 prediction = prediction.argmax()
 print(labels[prediction])
 
-#triangle
+if (torch.cuda.is_available()):
+	img2 = img2.cuda()
 prediction = simpl(img2)
 prediction = prediction.argmax()
 print(labels[prediction])
 
-#O
+if (torch.cuda.is_available()):
+	img3 = img3.cuda()
 prediction = simpl(img3)
 prediction = prediction.argmax()
 print(labels[prediction])
-
-#TR
-prediction = simpl(img4)
-prediction = prediction.argmax()
-print(labels[prediction])
-
-#triangle
-prediction = simpl(img6)
-prediction = prediction.argmax()
-print(labels[prediction])
-
-#kvadrat
-prediction = simpl(img7)
-prediction = prediction.argmax()
-print(labels[prediction])
-'''
